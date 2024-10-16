930ca90435425eb9bf364f234ac14129f6f4ef38
Uttam sharma <uttamftspl@gmail.com>
2024-10-16 12:20:35 +0530

    datatypes

---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,18 @@
+// alert(3+3);  //we are using node JS not browser
+let name = "uttam"
+let age = 25;
+let isLoggedIN = false;
+
+//number = 2 to power 53
+//bigint = greater than number
+// String = ""
+// boolean => true/false
+// null = standalone value (empty)
+// undefined => no value assinged yet
+// symbol => unique
+
+// object
+
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
