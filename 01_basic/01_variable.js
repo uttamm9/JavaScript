8103edd2b2823742ebf5fbed3fafb6be02a36b67
Uttam sharma <uttamftspl@gmail.com>
2024-10-16 12:01:47 +0530

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountID = 14452
+let accountEmail = "uttam@gmail.com"
+var accountPassword = "09876"
+accountCity = "Jaipur"
+
+// console.log(accountID);
+accountEmail = "sharma@gmail.com"
+accountPassword = "554321"
+accountCity = "delhi"
+let accountstat;
+console.table([accountID,accountEmail,accountPassword,accountCity,accountstat ])
+
