54cc10b423a5c9c57466de07988a88103efc653f
Uttam sharma <uttamftspl@gmail.com>
2024-10-17 23:50:35 +0530

    Type conversion opration

---
+++ 01_basic/03_conversionOpration.js
@@ -0,0 +1,24 @@
+ let score = "23dgt";
+
+ console.log(typeof score)
+
+ let valueInNumber =  Number(score)  //data type conversion
+ console.log(typeof valueInNumber)
+ console.log(valueInNumber) // NaN means "Not A Number"
+
+// "33" => 33
+// "33asd" => NaN
+// true => 1, false => 0;
+
+let isLoggendIN = 1; //if enyting is thare the result is true otherwise in empty or 0 is flase;
+let booleanIsLoggendIN = Boolean(isLoggendIN)
+console.log(booleanIsLoggendIN) // true
+// 1=> true;
+// 0 => false;
+// "" => false;
+// "uttam" => true;
+
+let someNumber = 33;
+let stringNumber = String(someNumber) //convert to string
+console.log(stringNumber) // "33"
+console.log(typeof stringNumber)
