b3acf6d6272d20b6dd1225c54fbf8e72d1b16055
Uttam sharma <uttamftspl@gmail.com>
2024-10-18 21:41:28 +0530

    comparision of datetypes

---
+++ 01_basic/04_Comparision.js
@@ -0,0 +1,9 @@
+console.log(1>2); //false
+console.log(1!=2); // true
+
+console.log("2">1); // true > auto convert string to number
+console.log(null > 0) //false
+console.log(null < 0) // flase
+console.log(null >= 0) //ture null convert to 0 couse of comparision oprater
+// ===
+console.log("2" === 2) // flase also chekc datatype
