c0b3655e8a0102eef1b4e4cdbb037df76c50b5ee
Uttam sharma <uttamftspl@gmail.com>
2024-10-30 23:09:47 +0530

    object oprations

---
+++ 02_basic/04_objects.js
@@ -0,0 +1,30 @@
+const tinderUser = new Object() // singleton object
+// const tinderUser = {}// non singleton object
+
+tinderUser.id = "12fwef"
+tinderUser.name = "uttamm"
+tinderUser.isLOggendin = false;
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "someone@gmial.com",
+  fullname: {
+    first: "John",
+    last: "Doe"
+  }
+}   
+// console.log(regularUser.fullname.first); // john
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+// const obj3 = Object.assign({},obj1,obj2) //{}(enpty peranthis) is Target remain are source
+
+const obj4 = {...obj1,...obj2} //...(spred oprater)   
+// console.log(obj4);
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));//get all keys in an Array
+console.log(Object.values(tinderUser));//get all values in an Array
+
+
