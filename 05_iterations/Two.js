eef376012e23d756715aa645e33f6eba09b70ca8
Uttam sharma <uttamftspl@gmail.com>
2024-11-18 23:28:28 +0530

    while & do while loop

---
+++ 05_iterations/Two.js
@@ -0,0 +1,21 @@
+// while loop
+
+// let index = 0;
+// while (index<=10) {
+//   console.log(`VaLue of index is ${index}`)
+//   // index = index + 2;//increase index value from 2
+// }
+
+// let myArray = ["man","superma","batman","ironman","woman"]
+// let arr = 0;
+// while (arr<myArray.length){
+//   console.log(`value is ${myArray[arr]}`);
+//   arr = arr + 1; //increase i vlaue freom 1
+// }
+
+let score = 1
+
+do {
+  console.log(`score is ${score}`)
+  score++;
+} while (score <=10 );
