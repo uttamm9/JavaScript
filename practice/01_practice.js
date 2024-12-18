33cb7a028dc248bd68bbdb9db76fd3764d338078
Uttam sharma <uttamftspl@gmail.com>
2024-12-18 23:25:27 +0530

    get half string

--- practice/01_practice.js
+++ practice/01_practice.js
@@ -4,3 +4,9 @@ const equalTo100 = (a,b) => a===100 || b===10 || (a+b)===100
 console.log(equalTo100(100,0))
 console.log(equalTo100(100,100))
 console.log(equalTo100(50,50))
+
+const halfstr =  (str)=> str.slice(0,str.length/2)
+
+console.log(halfstr('raja'))
+console.log(halfstr('abcde'))
+console.log(halfstr('abcdef'))
