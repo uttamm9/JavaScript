e9cbd1cd90507ada9bdc972ee7b706e1722711d2
Uttam sharma <uttamftspl@gmail.com>
2024-12-24 21:43:52 +0530

    count even up to number

--- practice/math.js
+++ practice/math.js
@@ -7,3 +7,16 @@ const countevenNumber = (arr)=>arr.filter(num=>num%2===0).length
 
 console.log(countevenNumber([1,2,3,2,4,3,4,3,5,4,6]))
 console.log(countevenNumber([1,2,3,2,4,3,4,3,5,7,7]))
+
+//write a JS program to find the numbers of even values up to a given number
+
+const countEven = (array)=> array.filter(num=>num%2===0).length
+
+const arrayofNumbers = num =>{
+  const returnArray = []
+  for(let i=1;i<=num;i++){
+    returnArray.push(i)
+  }
+  return returnArray
+}
+console.log(countEven(arrayofNumbers(12)))
