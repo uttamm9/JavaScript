12c970be836eb1ef060ba9a0aeaf2d946b359e93
Uttam sharma <uttamftspl@gmail.com>
2024-12-30 21:01:58 +0530

    convert CSV string to 2D array

--- practice/string.js
+++ practice/string.js
@@ -9,4 +9,15 @@ const contain2to4 = (str, char) => {
   return count >= 2 && count <= 4;
 }
 
-console.log(contain2to4('JavaScript', 'a'));
+// console.log(contain2to4('JavaScript', 'a'));
+
+//Q. convert a comma-seprated-value(CSV) string to a 2D array.
+
+const converCSV = (str)=>{
+ return str.split('\n').map(row=>row.split(","));
+}
+
+let str = `abc,def,ghi
+jkl,mno,pqr
+stu,vwx,yza`;
+console.log(converCSV(str))
