133a24749d3fa7216c25332a722010d6573234fc
Uttam sharma <uttamftspl@gmail.com>
2025-02-08 23:36:50 +0530

    convert object to array and array to object

--- practice/Objects.js
+++ practice/Objects.js
@@ -10,3 +10,15 @@ const objectEqual = (a,b)=>{
 console.log(objectEqual(OBJ_1,OBJ_2))//true
 console.log(objectEqual(OBJ_1,OBJ_3))//false
 console.log(objectEqual(OBJ_2,OBJ_3))//false
+
+//Q. convert array of objects.
+  const array = ['a','b','c']
+
+  let obj = array.reduce((a,cur,i)=>({...a,[i]:cur}),{})
+  console.log(obj)//{a: 0, b: 1, c: 2}
+
+//Q. convert object to array
+  const obj1 = {a:1,b:2,c:3}
+
+  let array1 = Object.keys(obj).map(key=>obj[key])
+  console.log(array)//[1,2,3]
